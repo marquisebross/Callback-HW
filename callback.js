678c813bf580fc9dd6d696dc2418f5651bd9da99
MARQUISE ROSS <marquiseb_ross@yahoo.com>
2021-05-03 18:02:50 -0500

    first commit

---
+++ callback.js
@@ -0,0 +1,6 @@
+// Asynchronous callback function calling message after button is clicked.
+
+document.querySelector("#btn")
+.addEventListener("click", function() {
+    console.log("Thank You For Submitting Your Payment");
+});
